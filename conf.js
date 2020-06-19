a29e0ec297ef22c88ff7a0f896a06913f93c45be
magb7 <margaux.bedu@gmail.com>
2020-06-19 09:35:46 +0200

    rename export

--- conf.js
+++ conf.js
@@ -2,10 +2,10 @@ const mysql = require("mysql");
 require("dotenv").config();
 const { DB_USER, DB_PASSWORD, DB_DATABASE, DB_HOST } = process.env;
 
-const connection = mysql.createConnection({
+const connexion = mysql.createConnection({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_DATABASE,
 });
-module.exports = connection;
+module.exports = connexion;
